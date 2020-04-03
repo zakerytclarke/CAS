fe5ffde7db04094a79421bc2f423a788aa6eeef7
Zakery Clarke <zakerytclarke@gmail.com>
2020-04-02 23:16:56 -0600

    Simple Derivatives

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports.solve=solve;
+module.exports.derive=derive;
+module.exports.findCriticalPts=findCriticalPts;
 
 //Solve for a given variable in an equation
 function solve(varName,equation){
@@ -84,6 +86,9 @@ function simplify(equation){
   var arg1=equation[1];
   var arg2=equation[2];
 
+  if(op=="/"&&compare(arg1,arg2)){
+    return 1;
+  }
 
   //Simplifying at Top Level
 
@@ -102,6 +107,14 @@ function simplify(equation){
   }
 
   //Identity
+  if(op=="+"){
+    if(arg1==0){
+      return arg2;
+    }
+    if(arg2==0){
+      return arg1;
+    }
+  }
   if(op=="*"){
     if(arg1==1){
       return arg2;
@@ -323,6 +336,81 @@ function contains(equation,varName){
 
 //Checks if equations are equal
 function compare(eq1,eq2){
-  console.log(JSON.stringify(eq1)==JSON.stringify(eq2));
-  JSON.stringify(eq1)==JSON.stringify(eq2);
+  return JSON.stringify(eq1)==JSON.stringify(eq2);
+}
+
+
+/**
+ * Finds a derivative of var1 with respect to var2 in eq
+ */
+function derive(var1,var2,eq){
+  var equation=solve(var1,eq);
+  var side1=equation[1];
+  var side2=equation[2];
+  return [equation[0],side1,derivative(var1,var2,side2)];
+}
+
+
+function derivative(var1,var2,eq){
+
+    if(eq.constructor===Number){
+      return 0;
+    }
+    if(eq.constructor===String){
+      return 1;
+    }
+
+
+
+    var op=eq[0];
+    var side1=eq[1];
+    var side2=eq[2];
+
+    //Basic Derivatives
+    if(op=="*"){
+      if(side1.constructor===Number&&side2.constructor===String){
+        return side1;
+      }
+      if(side1.constructor===String&&side2.constructor===Number){
+        return side2;
+      }
+    }
+
+    if(op=="/"){
+      if(side1.constructor===Number&&side2.constructor===String){
+        return 1/side1;
+      }
+      if(side1.constructor===String&&side2.constructor===Number){
+        return 1/side2;
+      }
+    }
+
+    if(op=="^"){
+      if(side1.constructor===String&&side2.constructor===Number){
+        return ["*",side2,side1];
+      }
+    }
+
+    //Composed Derivatives
+    if(op=="+"){
+      return simplify(["+",derivative(var1,var2,side1),derivative(var1,var2,side2)]);
+    }
+    if(op=="-"){
+      return simplify(["-",derivative(var1,var2,side1),derivative(var1,var2,side2)]);
+    }
+}
+
+
+/**
+ * Finds the critical points by
+ * determining where the derivative
+ * is 0
+ */
+function findCriticalPts(var1,var2,eq){
+  var d=derive(var1,var2,eq);
+  var zeroFunc=simplify(substitute(d,var1,0));
+  var solved=simplify(solve(var2,zeroFunc));
+  var xVal=solved[2];
+  var substitutedWithXVal=simplify(substitute(eq,var2,xVal));
+  return substitutedWithXVal;
 }
--- test.js
+++ test.js
@@ -1,35 +1,60 @@
 var CAS=require("./index.js");
 
 var tests=[
-  [["=","y","x"],["=","y","x"]],
-  [["=",["*",2,"y"],"x"],["=","y",["/","x",2]]],
-  [["=",2,"y"],["=","y",2]],
-  [["=",["^","y",2],"x"],["=","y",["^","x",0.5]]],
-  [["=",["^","y",0.5],"x"],["=","y",["^","x",2]]],
-  [["=","y",["+","x","x"]],["=","y",["*","x",2]]],
-  [["=","y",["*","x","x"]],["=","y",["^","x",2]]],
-  [["=",["+","y","x"],1],["=","y",["-",1,"x"]]],
-  [["=",["-","y","x"],1],["=","y",["+",1,"x"]]],
-  [["=",["*","x","y"],1],["=","y",["/",1,"x"]]],
-  [["=",["/","x","y"],1],["=","y","x"]],
-  [["=","y",["/",["^","x",2],["^","x",2]]],["=","y",1]],
-  [["=",["^",["+","y",1],0.5],["^",["+","x",17],2]],[]],
-  [["=",["+","y",1],"x"],["=","y",["-","x",1]]]
+  //Test Solver
+  {input:["=","y","x"],output:["=","y","x"],function:"solve"},
+  {input:["=",["*",2,"y"],"x"],output:["=","y",["/","x",2]],function:"solve"},
+  {input:["=",2,"y"],output:["=","y",2],function:"solve"},
+  {input:["=",["^","y",2],"x"],output:["=","y",["^","x",0.5]],function:"solve"},
+  {input:["=",["^","y",0.5],"x"],output:["=","y",["^","x",2]],function:"solve"},
+  {input:["=","y",["+","x","x"]],output:["=","y",["*","x",2]],function:"solve"},
+  {input:["=","y",["*","x","x"]],output:["=","y",["^","x",2]],function:"solve"},
+  {input:["=",["+","y","x"],1],output:["=","y",["-",1,"x"]],function:"solve"},
+  {input:["=",["-","y","x"],1],output:["=","y",["+",1,"x"]],function:"solve"},
+  {input:["=",["*","x","y"],1],output:["=","y",["/",1,"x"]],function:"solve"},
+  {input:["=",["/","x","y"],1],output:["=","y","x"],function:"solve"},
+  {input:["=","y",["/",["^","x",2],["^","x",2]]],output:["=","y",1],function:"solve"},
+  {input:["=",["^",["+","y",1],0.5],["^",["+","x",17],2]],output:["=","y",["-",["^",["+","x",17],4],1]],function:"solve"},
+  {input:["=",["+","y",1],"x"],output:["=","y",["-","x",1]],function:"solve"},
+  {input:["=",1,["^","x","y"]],output:["=","y",["/",["ln","1"],["ln","x"]]],function:"solve"},
+  //{input:,output:,function:"solve"},
+  //Test Derivative
+  {input:["=","y",1],output:["=","y",0],function:"derive"},
+  {input:["=","y","x"],output:["=","y",1],function:"derive"},
+  {input:["=","y",["*",2,"x"]],output:["=","y",2],function:"derive"},
+  {input:["=","y",["^","x",2]],output:["=","y",["*",2,"x"]],function:"derive"},
+  {input:["=","x",["*",0.5,"y"]],output:["=","y",2],function:"derive"},
+  {input:["=","y",["+",2,"x"]],output:["=","y",1],function:"derive"},
+  {input:["=","y",["+",["^","x",2],5]],output:["=","y",["*",2,"x"]],function:"derive"},
+  //["=","y",["^","y",2],1]
 
+  //Finding Critical Points
+  {input:["=","y",["^","x",2]],output:["=","x",0],function:"criticalPts"},
+  {input:["=","y",["+",["^","x",2],5]],output:["=","x",5],function:"criticalPts"},
 ];
 
 
 
 for(var i=0;i<tests.length;i++){
-  var test=tests[i][0].slice();
-  var result=CAS.solve("y",test);
-  var status=compare(tests[i][1],result);
-  console.log("Test "+i,"Status: ",status);
-    console.log("  ","Input: "+JSON.stringify(tests[i][0]));
-    console.log("  ","Output: "+JSON.stringify(result));
-    console.log("  ","Expected: "+JSON.stringify(tests[i][1]));
-    if(status==false){
+  var test=tests[i].input.slice();
+  if(tests[i].function=="solve"){
+    var result=CAS.solve("y",test);
   }
+  if(tests[i].function=="derive"){
+    var result=CAS.derive("y","x",test);
+  }
+  if(tests[i].function=="criticalPts"){
+    var result=CAS.findCriticalPts("y","x",test);
+  }
+
+  var status=compare(tests[i].output,result);
+  console.log("Test "+i,"Status: ",status);
+  if(status==false){
+    console.log("  ","Input: "+JSON.stringify(tests[i].input));
+    console.log("  ","Output: "+JSON.stringify(result));
+    console.log("  ","Expected: "+JSON.stringify(tests[i].output));
+  }
+
 }
 
 
